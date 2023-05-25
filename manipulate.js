16bb26e186c03167c746744f7f588644b6b8f0ba
Sudhi Krishna N A <sudhi27krishna@gmail.com>
2023-05-25 10:34:49 +0530

    Data changed in manipulate.js

--- manipulate.js
+++ manipulate.js
@@ -1,39 +1,28 @@
 const { spawn } = require('node:child_process');
 
 const data = {
-    date: '12-06-2023',
+    date: '17-06-2023',
     time: 'FN',
     rooms: [
-      { room_no: 'M111', capacity: 30 },
-      { room_no: 'M105', capacity: 30 },
-      { room_no: 'M113', capacity: 30 },
-      { room_no: 'M204', capacity: 30 },
-      { room_no: 'M205', capacity: 30 },
-      { room_no: 'M207', capacity: 30 },
-      { room_no: 'M209', capacity: 30 },
-      { room_no: 'M213', capacity: 30 },
-      { room_no: '209', capacity: 30 },
-      { room_no: '211', capacity: 30 },
-      { room_no: '401', capacity: 30 },
-      { room_no: '404', capacity: 30 },
-      { room_no: '502', capacity: 30 },
-      { room_no: '503', capacity: 30 },
-      { room_no: '505', capacity: 30 },
-      { room_no: '506', capacity: 30 },
-      { room_no: 'M229', capacity: 30 },
-      { room_no: 'M211', capacity: 40 },
-      { room_no: '403', capacity: 40 }
+        { room_no: 'M111', capacity: 30 },
+        { room_no: 'M105', capacity: 30 },
+        { room_no: 'M113', capacity: 30 },
+        { room_no: 'M204', capacity: 30 },
+        { room_no: 'M205', capacity: 30 },
+        { room_no: 'M207', capacity: 30 },
+        { room_no: 'M209', capacity: 30 },
+        { room_no: 'M213', capacity: 30 },
+        { room_no: '209', capacity: 30 },
+        { room_no: '211', capacity: 30 },
+        { room_no: '401', capacity: 30 }
     ],
     details: [
-      { sem: 3, branch: 'CS', slot: 'A', subcode: 'MAT203' },
-      { sem: 3, branch: 'CA', slot: 'A', subcode: 'MAT203' },
-      { sem: 3, branch: 'AD', slot: 'A', subcode: 'MAT203' },
-      { sem: 3, branch: 'EE', slot: 'A', subcode: 'MAT201' },
-      { sem: 3, branch: 'EC', slot: 'A', subcode: 'MAT201' },
-      { sem: 3, branch: 'ME', slot: 'A', subcode: 'MAT201' },
-      { sem: 3, branch: 'CE', slot: 'A', subcode: 'MAT201' }
+        { sem: 3, branch: 'EE', slot: 'B', subcode: 'EET201' },
+        { sem: 3, branch: 'EC', slot: 'B', subcode: 'ECT201' },
+        { sem: 3, branch: 'ME', slot: 'B', subcode: 'MET201' },
+        { sem: 3, branch: 'CE', slot: 'B', subcode: 'CET201' }
     ]
-  };
+};
 
 const manipulate = () => {
     // Convert the data object to JSON string
